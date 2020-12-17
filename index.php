1d1083d5a29cdcd46e86c85a4e70313bfbf43f22
chabelitabalaan1 <73258713+chabelitabalaan1@users.noreply.github.com>
2020-12-17 10:48:40 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+include_once("home.html");
+
+alert("KEEP CALM AND ENJOY COOKING!!")
+
+function alert($msg){
+    echo "<script type='text/javascript'>alert('$msg');</script>";
+    
+}
+?>
