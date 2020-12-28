551e9b71046cd399e8b831f3870146ead9f2971a
chabelitabalaan1 <73258713+chabelitabalaan1@users.noreply.github.com>
2020-12-29 04:36:22 +0800

    changes style

--- main.js
+++ main.js
@@ -1,10 +1,5 @@
-var counter =1;
-setInterval(function(){
-            document.getElementById('radio + counter').checked = true;
-counter++;
-if(counter > 4){
-    counter = 1;
-}
-
-
-            }, 5000);
+ var swiper = new Swiper('.swiper-container', {
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
